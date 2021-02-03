3056a0b306426721b2208d1e014652baba009fe6
jneal <neal.justin91@gmail.com>
2021-02-02 17:48:30 -0700

    Changing the color of the rotating image to orange

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Rob Starkovich is a scrub.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
